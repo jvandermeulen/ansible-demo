75621f57843754cc3097fbf91c353b0d6a513f09
Jorgen van der Meulen <jur.public@xs4all.nl>
2017-03-07 16:48:27 +0100

    bugfixing

--- roles/ncpa/files/service_active.sh
+++ roles/ncpa/files/service_active.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Script:       hostname.sh
-# Purpose:      show only hostname (check whether you're calling NCPA from the expected server) as well as SElinux status
-echo "Server: $(/usr/bin/hostname) with SELinux in $(getenforce) mode. Mode from config file: $(sestatus | awk '/Mode from config file/ {print $NF}')"
-[root@tst-vsu-apl-004 plugins]# acat^C
-[root@tst-vsu-apl-004 plugins]# cat service_active.sh
-#!/bin/bash
 # Script:       service_active.sh
 # Author:       Conclusion Xforce
 # Version:      0.1 Jorgen: initial version of check for systemd service (expects one argument)
